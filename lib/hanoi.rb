2c010798a50a64b11814c716232b3a83681ed129
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2015-03-16 23:38:20 +0000

    Push fake down: introduce getSpareColumn function

--- lib/hanoi.rb
+++ lib/hanoi.rb
@@ -7,9 +7,13 @@ def moveNPieces n, fromTo
 
   from = fromTo[:from]
   to = fromTo[:to]
-  spareColumn = 1
+  spareColumn = getSpareColumn fromTo
 
   moveNPieces(n - 1, {from: from, to: spareColumn}) +
     moveNPieces(1, {from: from, to: to}) +
     moveNPieces(n - 1, {from: spareColumn, to: to})
+end
+
+def getSpareColumn fromTo
+  1
 end
